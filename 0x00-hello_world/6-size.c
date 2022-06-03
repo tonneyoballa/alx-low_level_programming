957311391e308d8edfdde03be47dea325e961548
tonneyoballa <tonneyoballah@gmail.com>
2022-06-03 19:11:15 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,6 @@ int main(void)
 
 	int long c;
 	int long long d;
-
 	printf("Size of a char: %zu byte(s)\n", sizeof(a));
 	printf("Size of an int: %zu btye(s)\n", sizeof(b));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
