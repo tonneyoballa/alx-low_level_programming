fcdd8b1df197eeda6597f553348d8f5b30f95237
tonneyoballa <tonneyoballah@gmail.com>
2022-06-06 01:26:52 -0700

    3print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(vaid)
+{
+char alpha;
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+putchar(alpha);
+for (alpha = 'A'; alpha <= 'Z'; alpha++)
+putchar(alpha);
+putchar('\n');
+return (0);
+}
