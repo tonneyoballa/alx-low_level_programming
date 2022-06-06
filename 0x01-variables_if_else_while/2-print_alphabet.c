fc95bdc24252382041262645b0a622784e18fa82
tonneyoballa <tonneyoballah@gmail.com>
2022-06-06 00:56:48 -0700

    2-prnt alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha;
+
+for (alpha = 'a'; aplpha <= 'z'; alpha++)
+putchar(alpha);
+putchar('\n');
+return (0);
+}
