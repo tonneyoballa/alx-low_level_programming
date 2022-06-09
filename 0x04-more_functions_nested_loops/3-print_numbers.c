4899223cdd4c455100de44b94ea8cedff1f0b0be
tonneyoballa <tonneyoballah@gmail.com>
2022-06-09 08:45:26 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ * Description: prints numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char j;
+
+	j = '0';
+	while (j <= '9')
+	{
+		_putchar(j + '0');
+		j++;
+	}
+	_putchar('\n');
+}
