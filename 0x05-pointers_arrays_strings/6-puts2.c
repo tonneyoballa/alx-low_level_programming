2987c5b58972cc8e3a5a4e73ff6ccea86c42ca4d
tonneyoballa <tonneyoballah@gmail.com>
2022-06-13 12:42:30 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * _puts2 - Prints pair values.
+ * @str: the string to be treated
+ *
+ * Return: 0
+ */
+void _puts2(char *str)
+{
+	int n;
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+
+	for (n = 0; n < a; n += 2)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
