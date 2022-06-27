d517ef197f5231e4c5a23a983d8fd991d4511d63
tonneyoballa <tonneyoballah@gmail.com>
2022-06-27 07:35:08 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,15 +17,12 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-
-	for (strlen = 0; str[strlen] != '\0'; strlen++)	
-     {
-        cpy_str = malloc((strlen + 1) * sizeof(char));
-     }
-
+	for (strlen = 0; str[strlen] != '\0'; strlen++)
+	{
+	cpy_str = malloc((strlen + 1) * sizeof(char));
+	}
 	if (cpy_str == NULL)
-		return (NULL);
-
+	return (NULL);
 	for (i = 0; i < strlen; i++)
 	{
 		cpy_str[i] = str[i];
