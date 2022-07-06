220053abdc767211cb40478fdd525527824b8f03
tonneyoballa <tonneyoballah@gmail.com>
2022-07-06 15:51:39 +0300

    fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,4 +10,19 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+    int x;
 
+	if (size <= 0)
+		return (-1);
+
+	if (!array || !size || !cmp)
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		if (cmp(array[x]))
+			return (x);
+	}
+
+	return (-1);
+}
