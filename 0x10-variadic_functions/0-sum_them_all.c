3b9a32ddcf9b9acc1417bec2b3013e4d85783a71
tonneyoballa <tonneyoballah@gmail.com>
2022-07-07 21:44:18 +0300

    c is hard

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,1 +1,24 @@
+#include "variadic_functions.h"
 
+/**
+ * sum_them_all -  returns the sum of all its parameters
+ * @n: input number
+ * 
+ * Return: 0
+ * /
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list xz;
+    unsigned int a, sum = 0;
+
+    if (n == 0)
+        return(0);
+
+    va_start(xz, n);
+
+    for (a = 0; a < n; a++)
+        sum += va_arg(xz, int);
+    
+    va_end(xz);
+    return(sum);
+}
