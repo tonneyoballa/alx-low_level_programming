32caf28e9820be510b72cef8dbb12bca3988e826
tonneyoballa <tonneyoballah@gmail.com>
2022-07-07 13:58:20 -0700

    fixxxed

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,23 +4,23 @@
  * print_numbers - prints numbers, followed by a new line
  * @n: number passed to the function
  * @separator: string to be printed
- * 
+ *
  * Return: void
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list xz;
-    unsigned int a;
+	va_list xz;
+	unsigned int a;
 
-    va_start(xz, n);
+	va_start(xz, n);
 
-    for (a = 0; a < n; a++)
-    {
-        printf("%d", (va_arg(xz, unsigned int)));
-        if ((a!= (n - 1)) && separator != NULL)
-                printf("%s", separator);
-    }
-    printf("\n");
+	for (a = 0; a < n; a++)
+	{
+		printf("%d", (va_arg(xz, unsigned int)));
 
-    va_end(xz);
+		if ((a != (n - 1)) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(xz);
 }
