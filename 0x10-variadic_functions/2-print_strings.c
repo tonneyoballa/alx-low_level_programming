fb468eec72615664123d420cd049dca3156bc57d
tonneyoballa <tonneyoballah@gmail.com>
2022-07-07 14:19:18 -0700

    big fixx

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,28 +7,30 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    va_list xz;
-    unsigned a;
-    char *str;
+	va_list xz;
+	unsigned int a;
+	char *str;
 
-    va_start(xz, n);
+	va_start(xz, n);
 
-    for (a = 0; a < n; a++)
-    {
-        str = (va_arg(xz, char *));
-        if (str != NULL)
-        {
-            printf("%s", str);
-        }
-    
-        else
-        {
-        printf("(nil)");
-        }
+	for (a = 0; a < n; a++)
+	{
+		str = (va_arg(xz, char *));
 
-        if ((a != (n - 1)) && separator != NULL)
-        printf("%s", separator);
-    }
-    printf("\n");
-    va_end(xz);
+		if (str != NULL)
+		{
+			printf("%s", str)
+		}
+
+		else
+
+		{
+			printf("(nil)");
+		}
+
+		if ((a != (n - 1)) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(xz);
 }
